ebbc359b09600c45e702489bb340ccfdd20f7660
Frank Epperlein <frank@epperle.in>
2017-08-10 12:56:37 +0200

    add basic setup scripts

---
+++ build-virtualenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+cd "${0%/*}"
+
+virtualenv env
+source ./env/bin/activate
+pip3 install wheel --upgrade
+pip3 install setuptools --upgrade
+python3 setup.py sdist
+python3 setup.py bdist_wheel
+deactivate
+rm -rf env/ build/ mtv_dl.egg-info
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+from pip.req import parse_requirements
+
+requirements = parse_requirements(os.path.join(os.path.dirname(__file__), 'requirements.txt'), session='dummy')
+
+setup(name='mtv_dl',
+      version='0.1',
+      description='MediathekView Downloader',
+      long_description='Commandline tool to download videos from MediathekView.',
+      author='Frank Epperlein',
+      author_email='frank+mtv_dl@epperle.in',
+      url='https://github.com/efenka/mtv_dl',
+      py_modules=['mtv_dl'],
+      entry_points={
+            'console_scripts': [
+                  'mtv-dl = mtv_dl:main',
+            ]
+      },
+      classifiers=[
+            'Topic :: Multimedia :: Video',
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'License :: Public Domain',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+      ],
+      python_requires='>=3.5',
+      install_requires=[str(ir.req) for ir in requirements])
+
